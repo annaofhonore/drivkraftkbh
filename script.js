fdec88b214718c790fc6ed2f5d819295f013fb8c
Jannick Holm <jannick@wonderfestiwall.dk>
2019-06-25 15:18:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,35 +37,35 @@ function toggleMenu() {
 }
 
 
-document.addEventListener("DOMContentLoaded", start);
+//document.addEventListener("DOMContentLoaded", start);
 
-function start(){
-    console.log("start");
-    async function getJson() {
-        console.log("getJson")
-let url="http://oliviahonore.dk/drivkraftkbh/wordpress/wp-json/wp/v2/service"
-let jsonData = await fetch(url);
-indhold = await jsonData.json();
-        visServices(); 
-    }
-    
-    getJson();
-    
-    
-    function visServices() {
-        console.log("services");
-        console.log(shop);
-        let dest = document.querySelector(".produkter-container");
-        let temp = document.querySelector("template");
-        services.forEach(service => {
-        
-            
-        
-        dest.lastElementChild.addEventListener("click", () => {
-                location.href = "services?titel=" + serivce.title.rendered
-            });
-    }
-    
-    
-    
-}
+//function start(){
+//    console.log("start");
+//    async function getJson() {
+//        console.log("getJson")
+//let url="http://oliviahonore.dk/drivkraftkbh/wordpress/wp-json/wp/v2/service"
+//let jsonData = await fetch(url);
+//indhold = await jsonData.json();
+//        visServices(); 
+//    }
+//    
+//    getJson();
+//    
+//    
+//    function visServices() {
+//        console.log("services");
+//        console.log();
+//        let dest = document.querySelector(".produkter-container");
+//        let temp = document.querySelector("template");
+//        services.forEach(service => {
+//        
+//            
+//        
+//        dest.lastElementChild.addEventListener("click", () => {
+//                location.href = "services?titel=" + serivce.title.rendered
+//            });
+//    });    
+//    
+//    
+//}
+//}
